d636914524d5862c9c2f3e227caba75864683ad5
Ryan Marks <Ryan@paleo2go@gmail.com>
2017-12-31 21:30:35 +0000

    39Changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,8 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
-
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
@@ -12,5 +11,4 @@ class PhotosController < ApplicationController
   def photo_params
     params.require(:photo).permit(:caption, :picture)
   end
-
 end
